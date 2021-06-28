0c50fe1dff92242989e2ce25c9d0ff9ec6bed8ae
Cristian Ordonez <v1vectorfile@gmail.com>
2021-06-28 13:50:00 -0500

    proyecto-1

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/utilidades/nabvar/nabvar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NabvarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/utilidades/nabvar/nabvar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nabvar',
+  templateUrl: './nabvar.component.html',
+  
+})
+export class NabvarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
